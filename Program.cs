308ccf9579107dbd3d7e6242db4018aaa30f7fde
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-01-24 15:07:47 -0600

    letter loops exercise

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input some random characters");
+            var characters = Console.ReadLine();
+            string newString = string.Empty;
+            var counter = 1;
+            foreach (char a in characters)
+            {
+                for (int i = 0; i < counter; i++)
+                {
+                    newString += a;
+                }
+                newString += "*";
+                counter++;
+            }
+            Console.WriteLine($"The new string {newString.TrimEnd('*')}");
+        }
+    }
+}
